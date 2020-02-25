0de6d63f0e5d9f380c01e15dabd773c99763b603
liho <tech.support@revenuemonster.my>
2020-02-25 17:53:35 +0800

    fix: allowed untrusted ssl certificate in wkwebview

--- PWAIosWrapper/ViewController.swift
+++ PWAIosWrapper/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import WebKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        let cred = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+        completionHandler(.useCredential, cred)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
 
+        
+        let url = URL(string: "https://192.168.0.188:8080/qrscan")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
 
 }
 
